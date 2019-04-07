7c49822edd59783119ce09ccf003a924e23e7cb8
CharlieWangSI <charlie.hongyu.wang@gmail.com>
2019-04-07 17:17:36 -0400

    basic apps

---
+++ main.js
@@ -0,0 +1,14 @@
+const greetings = require("./greeting.js");
+const math = require("./math.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+greetings.greetingSpanish();
+greetings.greetingIcelandic();
+
+math.addition(1,2);
+math.subtraction(4,2);
---
+++ math.js
@@ -0,0 +1,9 @@
+exports.addition = function(num1,num2){
+  var sum = num1+num2
+  console.log(sum);
+}
+
+exports.subtraction = function(num1,num2){
+  var diff = num1-num2
+  console.log(diff);
+}
